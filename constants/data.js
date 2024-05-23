9a095572812f4e956c5f125add43361de9f2706e
hpsil <togyssovatumar@gmail.com>
2024-05-23 16:13:11 +0600

    init commit

---
+++ constants/data.js
@@ -0,0 +1,87 @@
+const userData = {
+  githubUsername: "TogussovaTumar",
+  name: "Tumar Togussova",
+  designation: "Front-End Developer",
+  avatarUrl: "/avatar.JPG",
+  email: "tumar.togussova@narxoz.kz",
+  phone: "+7 777 521 6037",
+  address: "Almaty, Kazakhstan.",
+  projects: [
+    {
+      title: "AddBoard App",
+      link: "https://github.com/TogussovaTumar/addboard-app",
+      imgUrl: "/addboard.png",
+    },
+    {
+      title: "Form Validation",
+      link: "https://github.com/TogussovaTumar/pwabord-app",
+      imgUrl: "/apollo.jpg",
+    },
+    {
+      title: "SSR in React",
+      link: "https://github.com/TogussovaTumar/task9",
+      imgUrl: "/ssr.jpg",
+    },
+    {
+      title: "CRUD with Redux",
+      link: "https://github.com/TogussovaTumar/addboard-app",
+      imgUrl: "/redux.jpg",
+    },
+    {
+      title: "Webpack Configuration",
+      link: "https://github.com/TogussovaTumar/task9",
+      imgUrl: "/webpack.png",
+    },
+    {
+      title: "Middleware with Redux",
+      link: "https://github.com/TogussovaTumar/addboard-app",
+      imgUrl: "/middle.png",
+    },
+  ],
+  about: {
+    title:
+        "Я разработчица, которая обожает создавать продукты и веб-приложения, влияющие на миллионы жизней",
+    description: [
+      `Я начала разрабатывать полнофункциональные приложения с прихода в Университет Нархоз. Тогда я не знала, что такое Разработка. Я настолько без идей сейчас, что буквально плачу, пока пишу этот текст, потому что lorem текст уже не в моде с 2012 года.`,
+      `После изучения HTML и трудностей с CSS, у меня появилась блестящая идея использовать Bootstrap, чтобы не приходилось стилизовать всё самой, и - по очевидным причинам - если ты знала Bootstrap, ты была крутой.`,
+      `Но теперь я стала лучше. Я программирую на React, Node.js, MongoDB, MySQL, PostgreSQL, Django, Bootstrap, Tailwind, C++, PHP языках программирования и многих других. Хотя я едва знаю синтаксис (Псс, ChatGPT!).`,
+    ],
+
+    currentProject: "AddBoard App",
+    currentProjectUrl: "https://github.com/TogussovaTumar/addboard-app",
+  },
+  experience: [
+    {
+      title: "IT (Digital Management and Design)",
+      company: "Narxoz University",
+      year: "2021-2025",
+      companyLink: "https://narxoz.edu.kz",
+      desc: "Поступала в этот университет в 2021 году. На данный момент изучаю Front-End with React.js",
+    },
+    {
+      title: "Front-End Developer",
+      company: "Google, USA.",
+      year: "2023",
+      companyLink: "https://google.com",
+      desc: "Делаю работы по Front-End, а точнее React и Next.js",
+    },
+    {
+      title: "Front-End(Full)",
+      company: "Beeline,Bootcamp, USA.",
+      year: "2022",
+      companyLink: "https://bootcamp.beeline.kz/",
+      desc: " Проходила стажировку по напралению Front-End. Изучала HTML, CSS, JS, React.js",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/",
+  socialLinks: {
+    instagram: "https://instagram.com/t_t_togussva_",
+    twitter: "https://twitter.com/",
+    linkedin: "https://linkedin.com/",
+    github: "https://github.com/TogussovaTumar",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
